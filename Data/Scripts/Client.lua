8e1fda9a122c6168fa418e4bf4080f9debf20815
CommanderFoo <pixeldepth@gmail.com>
2021-10-11 10:12:12 +0100

    Update Client.lua

--- Data/Scripts/Client.lua
+++ Data/Scripts/Client.lua
@@ -52,24 +52,6 @@ for c = 1, cols do
 	end
 end
 
-local function deepcopy(object)
-    local lookup_table = {}
-    local function _copy(object)
-        if type(object) ~= "table" then
-            return object
-        elseif lookup_table[object] then
-            return lookup_table[object]
-        end
-        local new_table = {}
-        lookup_table[object] = new_table
-        for index, value in pairs(object) do
-            new_table[_copy(index)] = _copy(value)
-        end
-        return setmetatable(new_table, getmetatable(object))
-    end
-    return _copy(object)
-end
-
 local function is_alive(c, r)
 	if(c < 1 or c >= cols or r < 1 or r >= rows) then
 		return 0
@@ -79,8 +61,6 @@ local function is_alive(c, r)
 end
 
 function Tick(dt)
-	local grid_copy = deepcopy(grid)
-	
 	for c = 1, cols do
 		for r = 1, rows do
 			local total = 0
@@ -97,22 +77,20 @@ function Tick(dt)
 			total = total + is_alive(c + 1, r + 1)
 
 			if grid[c][r].state == 0 and total == 3 then
-				grid_copy[c][r].next_state = 1
+				grid[c][r].next_state = 1
 			elseif(grid[c][r].state == 1 and (total < 2 or total > 3)) then
-				grid_copy[c][r].next_state = 0
+				grid[c][r].next_state = 0
 			end
 		end
 	end
 
 	Task.Wait()
 
-	grid = deepcopy(grid_copy)
-
 	for c = 1, cols do
 		for r = 1, rows do
 			local col = grid[c][r].obj:GetColor()
 
-			if(grid[c][r].next_state == 0) then
+			if(grid[c][r].next_state == 0 and grid[c][r].state ~= grid[c][r].next_state) then
 				local t = YOOTIL.Tween:new(tween_in_time, { r = col.r, g = col.g, b = col.b }, { r = 0, g = 0, b = 0 })
 
 				t:on_change(function(v)
